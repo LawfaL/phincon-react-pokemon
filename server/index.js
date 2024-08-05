359c1da3046dfb41459af5677a871dde5836e568
Val <44112412+LawfaL@users.noreply.github.com>
2024-08-05 20:46:26 +0700

    fix: db

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ var cors = require('cors')
 
 const express = require("express")
 const app = express()
-const db = require('./infrastructure/database')
 
 app.use(express.json())
 app.use(cors())
@@ -12,10 +11,6 @@ const pokemonRouter = require('./interface/routes/pokemon.route');
 
 app.use("/api/v1/pokemon", pokemonRouter)
 app.use(express.static("dist"))
-
-db.authenticate()
-  .then(() => console.log("DB Connected"))
-  .catch((error) => console.log(error));
 
 app.use('*', (req, res, next) => {
     res.status(404).json({
