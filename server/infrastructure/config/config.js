0ac0e65e8b4a774499e23acac7dac3a136fe4e59
Val <44112412+LawfaL@users.noreply.github.com>
2024-08-05 21:27:10 +0700

    fix: config db

--- server/infrastructure/config/config.js
+++ server/infrastructure/config/config.js
@@ -3,10 +3,10 @@ const pg = require("pg")
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "postgres",
-    database: "phincon_pokemon_app",
-    host: "localhost",
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     port: 5432,
     dialect: "postgres",
     dialectModule: pg
